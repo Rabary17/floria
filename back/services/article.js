060cc855f35b455e72a16feb507c6cfab57683cd
solofo <solofo.ralitera@gmail.com>
2018-08-07 16:47:45 +0300

    Article service

--- back/services/article.js
+++ back/services/article.js
@@ -1,14 +1,16 @@
 const Db = require('./database');
 const fs = require('fs');
+const ClientException = require('../exceptions/ClientException');
+const CategorieRepository = require('../repository/category');
+const ArticleRepository = require('../repository/article');
 
 module.exports = {
-    findByUid(uid) {
-        return new Promise((resolv, reject) => {
-            fs.readFile('samples/article.json', 'utf8', function (err, data) {
-                if (err) throw err;
-                obj = JSON.parse(data);
-                resolv(obj);
-            });
-        });
+    async add(article) {
+        if(! article) throw new ClientException.BadRequestException();
+        return await ArticleRepository.insert(article);
+    },
+
+    async findByUid(uuid) {
+        return await ArticleRepository.findByUuid(uuid);
     }
 };
