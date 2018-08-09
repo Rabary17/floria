2d03415c51f9871f291e37cc9f3180b9efd853c4
Andrianina <rbrstart@gmail.com>
2018-08-09 16:36:41 +0300

    filtre

--- back/routes/article.js
+++ back/routes/article.js
@@ -2,6 +2,7 @@ const Express = require('express');
 const Router = Express.Router();
 const Response = require('../services/response');
 const Article = require('../services/article');
+const ArticleRepository = require('../repository/article');
 
 Router.post('/', async function(req, res, next) {
     try {
@@ -21,4 +22,29 @@ Router.get('/:articleuid', async function(req, res, next) {
     }
 });
 
+Router.get('/:categorie/:mensualite/:prix_min/:prix_max', async function(req, res, next) {
+    try {
+        const article = await Article.findByUid(req.params.articleuid);
+        res.status(200).json(article);
+    } catch(e) {
+        return Response.sendError(res, e);
+    }
+});
+
+Router.get('/search/:categorie/:mensualite/:prix_min/:prix_max', async function(req, res, next){
+    let params = {
+                  'cat' : req.params.categorie,
+                  'men' : req.params.mensualite,
+                  'prix_min' : req.params.prix_min,
+                  'prix_max': req.params.prix_max 
+                };
+    try {
+        const article = await ArticleRepository.search(params);
+        let results = res.status(200).json(article);
+        return results.map( result => ArticleRepository.toObject(result));
+        console.log(article);
+    } catch(e) {
+        return Response.sendError(res, e);
+    }
+}),
 module.exports = Router;
