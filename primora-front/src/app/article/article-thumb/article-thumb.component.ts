8b6527e1279532e9d0f1053b7692f51b1a57e3cc
solofo <solofo.ralitera@gmail.com>
2018-08-14 09:46:45 +0300

    Upload file remove

--- primora-front/src/app/article/article-thumb/article-thumb.component.ts
+++ primora-front/src/app/article/article-thumb/article-thumb.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../../../models/article';
 import { config } from '../../../config';
+import {MediaService} from "../../../services/media.service";
 
 @Component({
   selector: 'app-article-thumb',
@@ -10,13 +11,15 @@ import { config } from '../../../config';
 })
 export class ArticleThumbComponent implements OnInit {
   public article: Article;
-  constructor() {
+  constructor(
+    private mediaService: MediaService
+  ) {
   }
 
   ngOnInit() {
   }
 
   getImage(file) {
-    return `${config.url}/medias/${file}`;
+    return this.mediaService.getImage(file);
   }
 }
